d5dc8b981ebfdf42f568a82509379795024a7e0f
Kaihotsu Kotaro <kaihotsu.kotaro@gmail.com>
2021-06-17 17:43:57 +0900

    debug: 選択されたレコードが変わった際にソートがリセットされるバグを修正

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useMemo } from "react";
 
 import jsonData from "./data.json";
 import { CritiqueRecord } from "./types/Critique";
@@ -7,13 +7,18 @@ import CritiqueDetail from "./components/organisms/CritiqueDetail";
 import ChartZone from "./components/organisms/ChartZone";
 
 const App = () => {
-  const records: CritiqueRecord[] = jsonData.map(data=>{
-    const temp = data.Q3
-    data.Q3 = data.Q2
-    data.Q2 = data.Q1
-    data.Q1 = temp
-    return data
-  });
+  const records = useMemo<CritiqueRecord[]>(
+    () =>
+      jsonData.map((data) => {
+        const temp = data.Q3;
+        data.Q3 = data.Q2;
+        data.Q2 = data.Q1;
+        data.Q1 = temp;
+        return data;
+      }),
+    []
+  );
+  
   const [currentCritique, setCurrentCritique] = useState(records[0]);
 
   return (
