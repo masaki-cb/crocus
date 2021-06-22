0a320951fbedfca2565f652e647577b93b3967ea
Kaihotsu Kotaro <kaihotsu.kotaro@gmail.com>
2021-06-22 19:31:34 +0900

    build

--- src/components/organisms/CritiqueTable.tsx
+++ src/components/organisms/CritiqueTable.tsx
@@ -46,7 +46,7 @@ const CritiqueTable = ({ allData, onRowClick, currentItem, lang }: Props) => {
         const itemContent = {
           Header: (
             <span style={{ whiteSpace: "nowrap" }}>
-              {item}:{t(`${critiqueQuestion(i18n)[item].bodyShort}`)}
+              {item}:{`${critiqueQuestion(i18n)[item].bodyShort}`}
             </span>
           ),
           accessor: (row: CritiqueRecord): string => row[item].toFixed(1),
